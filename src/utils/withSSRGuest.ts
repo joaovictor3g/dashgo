71995c96900c4383d1974144b3f7f9b15da756cd
joaovictor3g <jvdias79797@gmail.com>
2022-02-06 14:25:19 -0300

    feat: high order function

---
+++ src/utils/withSSRGuest.ts
@@ -0,0 +1,24 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export function withSSRGuest<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies();
+    if (cookies["dashgo.token"]) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
